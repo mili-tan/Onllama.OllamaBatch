76949a74919dcfaa25abc59134bd901160dce45f
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2025-05-24 21:06:25 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 using OllamaSharp.Models.Chat;
 using System.Collections.Concurrent;
 using System.Text.Json;
+using OllamaSharp.Models;
 
 namespace Onllama.OllamaBatch
 {
     internal class Program
     {
         public static HttpClient httpClient = new HttpClient()
-            {BaseAddress = new Uri("http://xw-gpu:11434"), Timeout = TimeSpan.FromMinutes(10)};
+            {BaseAddress = new Uri("http://xw-gpu:11434"), Timeout = TimeSpan.FromMinutes(5)};
         public static OllamaApiClient client = new OllamaApiClient(httpClient);
 
         static void Main(string[] args)
@@ -21,25 +22,36 @@ namespace Onllama.OllamaBatch
             {
                 var req = JsonSerializer.Deserialize<Req>(line);
 
-                Console.WriteLine(req.custom_id);
-                req?.body.messages.Insert(0, new Message(ChatRole.System, "/no_think"));
-                var chat = new ChatRequest() { Model = "qwen3:1.7b", Messages = req?.body.messages, Stream = false };
+                Console.WriteLine("Q:" + req.custom_id);
+
+                if (long.Parse(req.custom_id.Split('-').Last()) <= 20293) continue;
+                //req?.body.messages.Insert(0, new Message(ChatRole.System, "/no_think"));
+                var chat = new ChatRequest()
+                    {Model = "hf-mirror.com/unsloth/GLM-4-9B-0414-GGUF:Q4_K_M", Messages = req?.body.messages, Stream = false, KeepAlive = "-1s"};
                 tasks.Add(Task.Run(async () =>
                 {
-                    await foreach (var item in client.ChatAsync(chat))
+                    try
                     {
-                        item.Message.Content = item.Message.Content?.Split("</think>").LastOrDefault()?.Trim();
-                        req.body.model = item.Model;
-                        req.body.messages.Add(item.Message);
-                        answers.Add(JsonSerializer.Serialize(req, new JsonSerializerOptions
+                        await foreach (var item in client.ChatAsync(chat))
                         {
-                            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-                            WriteIndented = false
-                        }));
+                            //item.Message.Content = item.Message.Content?.Split("</think>").LastOrDefault()?.Trim();
+                            req.body.model = item.Model;
+                            req.body.messages.Add(item.Message);
+                            answers.Add(JsonSerializer.Serialize(req, new JsonSerializerOptions
+                            {
+                                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                                WriteIndented = false
+                            }));
+                            Console.WriteLine("R:" + req.custom_id);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
                     }
                 }));
 
-                if (tasks.Count < 8) continue;
+                if (tasks.Count < 12) continue;
 
                 Task.WaitAll(tasks.ToArray());
                 tasks.Clear();
